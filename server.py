bd14ece1933e6839c0c4067cefb3cc595fed39ed
fylhs1 <andrijbikov134@gmail.com>
2024-08-29 16:28:50 +0000

    OK

--- server.py
+++ server.py
@@ -5,18 +5,16 @@ import time
 app = Flask(__name__)
 
 api_key = "dab2647b113b40fc264d2352a1a9a715"
-city = "Kyiv"
-weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
-local_url = 'http://localhost:5000/update'
+cities = ["Kyiv", "Khmelnytskyi"]
+#local_url = 'http://localhost:5000/update'
 
 weather_data = {
     "weather": "Error",
     "temperature": 0.0,
     "pressure": 0}
 
-@app.route('/current_weather', methods=['GET'])
-def get_weather():
-    global weather_data
+def get_weather(c):
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={c}&appid={api_key}"
     response = requests.get(weather_url)
     data = response.json()
     weather_data['weather'] = data['weather'][0]['description']
@@ -25,9 +23,17 @@ def get_weather():
     # Add logic to update other weather data if needed
     return jsonify(weather_data), 200
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/Kyiv_weather', methods=['GET'])
+def kyiv_weather():
+    return get_weather(cities[0])
+
+@app.route('/Khmelnytskyi_weather', methods=['GET'])
+def khmelnytskyi_weather():
+    return get_weather(cities[1])
+
+#@app.route('/')
+#def index():
+#    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
